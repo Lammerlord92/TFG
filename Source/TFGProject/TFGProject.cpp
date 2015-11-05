21f850904977c23c9ebba154a6e6c51541131ae0
lammerlord92 <lammerlord92@gmail.com>
2015-11-05 11:09:50 +0100

    Base

---
+++ Source/TFGProject/TFGProject.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TFGProject.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TFGProject, "TFGProject" );
